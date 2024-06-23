88b0b18d98fca98f43f514c82c8bb95946a80fc5
lcw <newlcw@naver.com>
2024-06-23 16:50:00 +0900

    완

--- 09div_mul_deci/test01.py
+++ 09div_mul_deci/test01.py
@@ -1,1 +1,14 @@
-print("hellowowrld")
+inputs = []
+while True:
+    nums = list(map(int, input().split()))
+    if nums == [0, 0]:
+        break
+    inputs.append(nums)
+
+for input_ in inputs:
+    if input_[1] % input_[0] == 0:
+        print("factor")
+    elif input_[0] % input_[1] == 0:
+        print("multiple")
+    else:
+        print("neither")
